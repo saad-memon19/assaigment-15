af16b09fcdd5959a247bf804d549483efbb13605
saad-memon19 <razasaad3445@gmail.com>
2024-11-09 03:17:20 +0500

    simole value cehck

---
+++ app.js
@@ -0,0 +1,49 @@
+// multiply
+function multiply() {
+    var value1 = prompt('inter first value')
+    var value2 = prompt('inter second value')
+    alert(value1 * value2 + ' Answer ')
+}
+
+
+//subtract
+function subtract() {
+    var value1 = prompt('inter first value')
+    var value2 = prompt('inter second value')
+    alert(value1 - value2 + ' Answer ')
+}
+
+
+//devide
+function divide() {
+    var value1 = prompt('inter first value')
+    var value2 = prompt('inter second value')
+    alert(value1 / value2 + ' Answer ')
+}
+
+//reminder
+function reminder() {
+    var value1 = prompt('inter first value')
+    var value2 = prompt('inter second value')
+    alert(value1 % value2 + ' Answer ')
+}
+
+
+//odd even
+function oddEven() {
+    var value = prompt('inter your value')
+
+    if (value % 2 === 0) {
+        alert('even number')
+    }
+    else {
+        alert('odd number')
+    }
+}
+//DATA TYPE
+function dataType() {
+    alert('Type of "Saad" ' + "String")
+    alert('Type of 19 ' + "Number")
+    alert('Type of True/False ' + "Bulian")
+
+}
